c26bbd598217d6f5032dc072c36351ab23bd562e
Evan Gertis <physevangertis@gmail.com>
2022-03-22 17:15:28 -0400

    remove z

--- Week3Modules/JavascriptModule/ObjectModule.js
+++ Week3Modules/JavascriptModule/ObjectModule.js
@@ -4,14 +4,22 @@ const person = {
     title: "instructor",
     duties: function teach(){
         console.log("is teaching")
-    }
+    },
+    favoriteFood: "peanut butter pie",
+    mood: function emotion() {
+        console.log("having fun");
+    },
+    motivation: 7.9
 }
 
 function objectModule() {
     console.log(person.fName);
     console.log(person.lName);
     console.log(person.title);
+    console.log(person.motivation);
+    console.log(person.favoriteFood);
     person.duties();
+    person.mood();
 }
 
 objectModule();
