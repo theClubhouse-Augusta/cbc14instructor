fef8ac46a5988921008d5090a6cd8053c156d6ea
Evan Gertis <physevangertis@gmail.com>
2022-04-13 17:13:36 -0400

    add modules

---
+++ Week5Module/JavascriptModule1.js
@@ -0,0 +1,16 @@
+// write a loop that makes seven calls to console.log
+// to output the following code
+/*
+#
+##
+###
+####
+#####
+######
+*/
+
+var s = "#"
+for(var i=0; i < 6; i++){
+    s+="#"
+    console.log(s);
+}
